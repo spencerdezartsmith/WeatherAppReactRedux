7c6cdc99954f2456e68e2f4b5553e23da08015b0
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-08-13 14:52:32 -0700

    Add page header

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar'
 import WeatherList from '../containers/weather_list'
+import PageHeader from '../components/page_header'
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <PageHeader />
         <SearchBar />
         <WeatherList />
       </div>
---
+++ src/components/page_header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default (props) => {
+  return (
+    <div className="jumbotron">
+      <h1>My Vacation Forecast</h1>
+      <p>Plan exactly what to pack for you upcoming vacation by entering in one city or multiple US cities to see a weekly weather forecast.</p>
+    </div>
+  )
+}
