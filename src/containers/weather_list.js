47cba254b9fb637c68d696e820898f3347ebd256
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-08-13 14:18:26 -0700

    Set up sparlines representation of forecasts

---
+++ src/containers/weather_list.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Chart from '../components/chart'
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name
+    const temps = cityData.list.map(weather => weather.main.temp)
+    const pressures = cityData.list.map(weather => weather.main.pressure)
+    const humidities = cityData.list.map(weather => weather.main.humidity)
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td><Chart data={temps} color='purple' /></td>
+        <td><Chart data={pressures} color='blue' /></td>
+        <td><Chart data={humidities} color='pink' /></td>
+      </tr>
+    )
+  }
+
+  render() {
+    return (
+      <table className='table table-hover'>
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather } // { weather: weather }
+}
+
+export default connect(mapStateToProps)(WeatherList)
